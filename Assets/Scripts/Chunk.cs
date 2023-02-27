7bbebf3f9704bb58fd8de311d49c941969c561b5
nebuloid28 <nebuloid28@gmail.com>
2023-02-27 20:13:49 +0900

    230227
    voxel 공부

---
+++ Assets/Scripts/Chunk.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 작성자 : Ragu
+// 최초작성일 : 2023-02-27
+
+public class Chunk : MonoBehaviour
+{
+    int vertexIndex = 0;
+
+    List<Vector3> vertices = new List<Vector3>();
+    List<int> triangles = new List<int>();
+    List<Vector2> uv = new List<Vector2>();
+
+    public MeshRenderer meshRenderer;
+    public MeshFilter meshFilter;
+
+    void Start()
+    {
+        VoxeltoChunk();
+        DrawMesh();
+    }
+
+    void VoxeltoChunk()
+    {
+        // 6방향의 면 그리기
+        for (int p = 0; p < 6; p++)
+        {
+            // 각 면의 삼각형 2개 그리기
+            for (int i = 0; i < 6; i++)
+            {
+                int triangleIndex = VoxelData.voxelTriangles[p, i];
+
+                vertices.Add(VoxelData.voxelVertices[triangleIndex]);
+                triangles.Add(vertexIndex);
+                uv.Add(VoxelData.voxelUv[i]);
+
+                vertexIndex++;
+            }
+        }
+    }
+
+    void DrawMesh()
+    {
+        // 메시에 데이터들 초기화
+        Mesh mesh = new Mesh
+        {
+            vertices = vertices.ToArray(),
+            triangles = triangles.ToArray(),
+            uv = uv.ToArray()
+        };
+        mesh.RecalculateNormals();
+
+        meshFilter.mesh = mesh;
+    }
+}
---
+++ Assets/Scripts/VoxelData.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 작성자 : Ragu
+// 최초작성일 : 2023-02-27
+
+public class VoxelData : MonoBehaviour
+{
+    public static readonly Vector3[] voxelVertices = new Vector3[8]
+    {
+        /*
+         *   7──────6
+         *  /|     /|
+         * 3──────2 |
+         * | |    | |
+         * | 4────|─5
+         * |/     |/
+         * 0──────1
+         */
+        //Front
+        new Vector3(0.0f, 0.0f, 0.0f), //0 LB
+        new Vector3(1.0f, 0.0f, 0.0f), //1 RB
+        new Vector3(1.0f, 1.0f, 0.0f), //2 RT
+        new Vector3(0.0f, 1.0f, 0.0f), //3 LT
+        //Back
+        new Vector3(0.0f, 0.0f, 1.0f), //4 LB
+        new Vector3(1.0f, 0.0f, 1.0f), //5 RB
+        new Vector3(1.0f, 1.0f, 1.0f), //6 RT
+        new Vector3(0.0f, 1.0f, 1.0f), //7 LT
+    };
+
+    public static readonly Vector3[] faceChecks = new Vector3[6]
+    {
+        new Vector3( 0.0f,  0.0f, -1.0f), //Back Face
+        new Vector3( 0.0f,  0.0f, +1.0f), //Front Face
+        new Vector3( 0.0f, +1.0f,  0.0f), //Top Face
+        new Vector3( 0.0f, -1.0f,  0.0f), //Bottom Face
+        new Vector3(-1.0f,  0.0f,  0.0f), //Left Face
+        new Vector3(+1.0f,  0.0f,  0.0f), //RIght Face
+    };
+
+    public static readonly int[,] voxelTriangles = new int[6, 6]
+    {
+        //버텍스의 순서는 LB-LT-RB -- RB-LT-RT
+        //버텍스 인덱스가 시계방향인 이유 : 외적의 방향이 화면방향으로 향하게됨
+
+        {0, 3, 1, 1, 3, 2}, //Back Face
+        {5, 6, 4, 4, 6, 7}, //Front Face
+        {3, 7, 2, 2, 7, 6}, //Top Face
+        {1, 5, 0, 0, 5, 4}, //Bottom Face
+        {4, 7, 0, 0, 7, 3}, //Left Face
+        {1, 2, 5, 5, 2, 6}, //RIght Face
+    };
+
+    public static readonly Vector2[] voxelUv = new Vector2[6]
+    {
+        /*
+         * voxelTris의 버텍스 인덱스 순서에 따른 UV좌표 데이터
+         * 
+         * LB-LT-RB  -- RB-LT-RT
+         *   3            3ㅡ2
+         *   |＼           ＼|
+         *   0ㅡ1            1
+         */
+        new Vector2(0.0f, 0.0f), //LB
+        new Vector2(0.0f, 1.0f), //LT
+        new Vector2(1.0f, 0.0f), //RB
+
+        new Vector2(1.0f, 0.0f), //RB
+        new Vector2(0.0f, 1.0f), //LT
+        new Vector2(1.0f, 1.0f), //RT
+    };
+}
